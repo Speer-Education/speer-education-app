f911dbbd1a9867a11569c5ad5cbe0a3d9bcd6a03
Ee-Hsin <eehsinkok777@gmail.com>
2021-06-02 00:03:50 +0800

    Created About Section and made it responsive

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -14,9 +14,10 @@ export default function Home() {
                         <h1 className="home-intro__content-title">Speer Education</h1>
                         <p>Connecting adept college students with passionate high schoolers to enhance their subject 
                             specific awareness</p>
-                        <a href="#about"><button>ABOUT US</button></a>
-                        <a href="#launch"><button>KEEP INFORMED</button></a>
-                        {/* 2 Buttons */}
+                        <div className="home-intro__button-container">
+                            <a href="#about"><button>ABOUT US</button></a>
+                            <a href="#launch"><button>KEEP INFORMED</button></a>
+                        </div>
                     </div>
                     <div className="home-intro__image">
                         <img src="https://static.s123-cdn-static-d.com/ready_uploads/svg/normal_604d33874cc33.svg" alt=""></img>
@@ -24,7 +25,26 @@ export default function Home() {
                 </div>
             </section>
             <section className="home-about" id="about">
-            
+                {/* Flexbox, H1 */}
+                <h1 className="home-about__heading">ABOUT</h1>
+                {/* Flexbox, Left Side text, Right Side image */}
+                <div className="home-about__container">
+                    <div className="home-about__content">
+                        <p>
+                        Speer is a mentorship platform for passionate economics students who wish to learn more about economics/business 
+                        from college mentors. We strive to hone students' passions in Economics, Business, and Finance, especially with 
+                        COVID-19 acting as an unnecessary barrier for high-school students wanting to inflict change upon society.
+                        </p>
+                        <br/>
+                        <p>
+                        Through our platform, you'll be able to connect with a wide range of mentors and hold conversations in numerous 
+                        categories such as internships, real world application, class recommendations, and much more, all for free!
+                        </p>
+                    </div>
+                    <div className="home-about__image">
+                        <img src="https://static1.s123-cdn-static-a.com/uploads/5219948/normal_60a25a8740cb6.svg" alt=""></img>
+                    </div>
+                </div>
             </section>
             <section className="home-team" id="team">
 
