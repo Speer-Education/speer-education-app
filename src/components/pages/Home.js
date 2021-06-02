4762d23ce20f7910dd8e7b73ac51457df5105830
Ee-Hsin <eehsinkok777@gmail.com>
2021-06-02 16:16:53 +0800

    Added Carousel/Slider thingy

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,53 @@
 import React from 'react';
 import Navbar from '../Navbar';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import './Home.css';
 
 export default function Home() {
+
+    const sliderSettings = {
+        dots: false,
+        infinite: true,
+        lazyLoad: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 200,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
+        cssEase: "linear",
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+      };
+
+    const carouselItems = [
+        {title: 'LSE', src: 'https://static1.s123-cdn-static-a.com/uploads/5219948/400_60a66de4946c3.png'},
+        {title: 'Cambridge', src: "https://concourse.global/wp-content/uploads/2019/05/university-of-cambridge-2-logo.png"},
+        {title: 'NorthWestern', src: 'https://static1.s123-cdn-static-a.com/uploads/5219948/400_60a66e2582a79.png'},
+        {title: 'Yale-NUS', src: 'https://static1.s123-cdn-static-a.com/uploads/5219948/400_60a66cae30cc1.png'},
+        {title: 'UCL' , src: 'https://static1.s123-cdn-static-a.com/uploads/5219948/400_60a673595dd47_filter_60a674db37e91.jpg'},
+        {title: 'University Of Warwick', src: 'https://static1.s123-cdn-static-a.com/uploads/5219948/400_60a66c5c94505.png'},
+        {title: 'University Of Sheffield', src: 'https://static1.s123-cdn-static-a.com/uploads/5219948/400_60a66ec223e5f_filter_60a672e3bdda1.png'}
+    ]
+
     return (
         <div className="home">
             <Navbar/>
@@ -67,7 +112,12 @@ export default function Home() {
                 </div>
             </section>
             <section className="home-universities" id="universities">
-
+                <h1 className="home-universities__heading">UNIVERSITIES</h1>
+                <Slider className="home-universities__carousel" {...sliderSettings}>
+                    {carouselItems.map(item => (
+                        <img className="home-universities__carousel-images" key={item.title} src={item.src} alt={item.title}></img>
+                    ))}
+                </Slider>
             </section>
             <section className="home-launch" id="launch">
 
