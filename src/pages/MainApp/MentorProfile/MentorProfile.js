96f8730bea7003c896f1c4ed2d15fef95e540766
Chew Tzi Hwee <chewtzihwee@gmail.com>
2021-06-11 22:45:33 +0800

    Added loading for mentor document

--- src/pages/MainApp/MentorProfile/MentorProfile.js
+++ src/pages/MainApp/MentorProfile/MentorProfile.js
@@ -12,16 +12,18 @@ const MentorProfile = () => {
     const [mentor, setMentor] = useState();
     const [profPic, setProfPic] = useState();
     const [creatingRoom, setCreatingRoom] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     const { user } = useAuth();
 
     useEffect(() => {
-
+        setLoading(true)
         console.log(`mentors/${mentorId}`)
         return db.doc(`mentors/${mentorId}`).onSnapshot(async snap => {
             console.log(snap.data())
             setMentor(snap.data());
             setProfPic(await storage.ref(`/profilepics/${mentorId}.png`)?.getDownloadURL());
+            setLoading(false)
         })
     }, [mentorId]);
 
@@ -39,13 +41,15 @@ const MentorProfile = () => {
 
     return (
         <div className="mentorProfile">
-            {/* *Insert Photo (Have to search storage for it) */}
-            <img src={profPic} alt="mentor profile" />
-            <h1>{mentor?.name}</h1>
-            <h2>{mentor?.university}</h2>
-            <h2>{mentor?.major}</h2>
-            <p>{mentor?.description}</p>
-            <Button varient="default" disabled={creatingRoom} onClick={connectWithMentor}>{creatingRoom ? <Loader /> : "Connect With Mentor!"}</Button>
+            {loading ? <Loader />: <>
+                {/* *Insert Photo (Have to search storage for it) */}
+                <img src={profPic} alt="mentor profile" />
+                <h1>{mentor?.name}</h1>
+                <h2>{mentor?.university}</h2>
+                <h2>{mentor?.major}</h2>
+                <p>{mentor?.description}</p>
+                <Button varient="default" disabled={creatingRoom} onClick={connectWithMentor}>{creatingRoom ? <Loader /> : "Connect With Mentor!"}</Button>
+            </>}
         </div>
     );
 }
