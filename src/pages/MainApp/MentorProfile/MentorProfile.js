4952c479e43e75a8d1b49ed53cb3c4cfac0bf5ff
Ee-Hsin <eehsinkok777@gmail.com>
2021-06-11 21:38:28 +0800

    making request to backend

--- src/pages/MainApp/MentorProfile/MentorProfile.js
+++ src/pages/MainApp/MentorProfile/MentorProfile.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { db, storage } from '../../../config/firebase';
+import { db, storage, functions } from '../../../config/firebase';
+import { useAuth } from '../../../hooks/useAuth';
 import Button from '@material-ui/core/Button'
 import './MentorProfile.css';
 
@@ -8,6 +9,8 @@ const MentorProfile = () => {
     const { mentorId } = useParams();
     const [mentor, setMentor] = useState();
     const [profPic, setProfPic] = useState();
+
+    const { user } = useAuth();
 
     useEffect(() => {
         return db.doc(`users/${mentorId}`).onSnapshot(async snap => {
@@ -17,11 +20,28 @@ const MentorProfile = () => {
     }, [mentorId]);
 
 
-    const connectWithMentor = () => {
+    const connectWithMentor = async () => {
         /* So first check if a chat room with only both users exists, if it does, instantly redirect to that room. (redirect to /main-app/messages/${roomId}) 
-        If not, create a room with the mentorId and the current user Id, then add that room to the user's document int he database, and then redirect to 
+        If not, create a room with the mentorId and the current user Id, then add that room to the user's document AND the mentor's document in the database, and then redirect to 
         that room (redirect to /main-app/messages/${roomId}) */
-        console.log("redirects user to /main-app/messages/*generated roomId*")
+
+        // mentor?.rooms.forEach((roomId) => {
+        //     db.doc(`rooms/${roomId}`).get().then(snap => {
+
+        //         //I need to check if the user is in the room with the mentor, AND if there are only 2 people in that room --> Redirect to main-app/messages/${roomId}
+        //         //Create a room with mentor Id and current user Id, then add room id into mentor and current user's document, then Redirect to /main-app/messages/${roomId}
+
+        //         const snapData = snap.data();
+        //         const isConnected = areTheyConnected(snapData);
+        //     })
+        // })
+
+        //So now we pass mentorId and userId to backend, it returns the created/already existed room id. And then front end redirects from there.
+
+        await functions.httpsCallable('createRoom')({ mentorId, userId : user.id })
+            .catch((error) => {
+                console.error(error)
+            })
     }
 
     return (
