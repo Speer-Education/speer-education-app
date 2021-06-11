6cbb3654e699a5f9f7bb35e455e3e225c512e790
Ee-Hsin <eehsinkok777@gmail.com>
2021-06-11 19:54:11 +0800

    set up mentor profile (creating and redirecting user to room functionality is still not done) and not really styled properly either

--- src/container/MainApp/MainApp.js
+++ src/container/MainApp/MainApp.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Switch, Route, useRouteMatch } from "react-router-dom";
 import { AppNavbar } from '../../components/AppNavbar/AppNavbar';
-import MentorsPage from '../../pages/MainApp/Mentors/Mentors';
 import BlogPage from '../../pages/MainApp/Blog/BlogPage';
 import Messages from '../../pages/MainApp/Messaging/Messaging';
-
+import MentorsPage from '../../pages/MainApp/Mentors/Mentors';
+import MentorProfile from '../../pages/MainApp/MentorProfile/MentorProfile';
 
 export default function MainApp() {
 
@@ -24,7 +24,7 @@ export default function MainApp() {
                 </Route>
                 {/* Mentor Profile Page */}
                 <Route path={`${path}/mentors/:mentorId`}> 
-                    <h1> this is a profile lol</h1>
+                    <MentorProfile/>
                 </Route>
                 {/* Chat Page */}
                 <Route path={`${path}/messages`}> {/* Fixed routing bug to do with "exact" */}
--- src/pages/MainApp/MentorProfile/MentorProfile.js
+++ src/pages/MainApp/MentorProfile/MentorProfile.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { db } from '../../../config/firebase';
+import { db, storage } from '../../../config/firebase';
+import Button from '@material-ui/core/Button'
+import './MentorProfile.css';
 
 const MentorProfile = () => {
     const { mentorId } = useParams();
-    const [mentor, setMentor] = useState({});
+    const [mentor, setMentor] = useState();
+    const [profPic, setProfPic] = useState();
 
     useEffect(() => {
-        return db.doc(`users/${mentorId}`).onSnapshot(snap => {
-            setMentor(snap.data())
+        return db.doc(`users/${mentorId}`).onSnapshot(async snap => {
+            setMentor(snap.data());
+            setProfPic(await storage.ref(`/profilepics/${mentorId}.png`)?.getDownloadURL());
         })
-    }, [mentorId, setMentor]);
+    }, [mentorId]);
+
+
+    const connectWithMentor = () => {
+        /* So first check if a chat room with only both users exists, if it does, instantly redirect to that room. (redirect to /main-app/messages/${roomId}) 
+        If not, create a room with the mentorId and the current user Id, then add that room to the user's document int he database, and then redirect to 
+        that room (redirect to /main-app/messages/${roomId}) */
+        console.log("redirects user to /main-app/messages/*generated roomId*")
+    }
 
     return (
         <div className="mentorProfile">
-            
+            {/* *Insert Photo (Have to search storage for it) */}
+            <img src={profPic} alt="mentor profile"/>
+            <h1>{mentor?.name}</h1>
+            <h2>{mentor?.university}</h2>
+            <h2>{mentor?.major}</h2>
+            <p>{mentor?.description}</p>
+            <Button varient="default" onClick={connectWithMentor}>Connect With Mentor!</Button>            
         </div>
     );
 }
