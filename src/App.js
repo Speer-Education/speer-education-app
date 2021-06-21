516e51afd0d8ab6a2719b0fbb81c4d90bf053a6f
Ee-Hsin <eehsinkok777@gmail.com>
2021-06-21 15:30:06 +0800

    Added security for routes if user not logged in

--- src/App.js
+++ src/App.js
@@ -4,25 +4,31 @@ import Login from "./pages/Login/Login";
 import Onboarding from "./pages/Onboarding/Onboarding";
 import MainApp from "./container/MainApp/MainApp";
 import AdminApp from "./container/AdminApp/AdminApp";
-import { AuthProvider } from "./hooks/useAuth";
-import { Router, Switch, Route } from "react-router-dom";
+import { useAuth } from "./hooks/useAuth";
+import { Router, Switch, Route, Redirect } from "react-router-dom";
 import history from './hooks/history';
 
 function App() {
+
+  const { user } = useAuth();
+
   return (
-    <AuthProvider>
-      <Router history={history}>
-        <div className="app">
-          <Switch>
-            <Route exact path="/" component={Home} />
+    <Router history={history}>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path={"/login"} component={Login} />
+          {user ? <>
             <Route path="/app" component={MainApp} /> {/* In the future, we make it so it only renders if user is logged in */}
             <Route path="/admin-app" component={AdminApp} /> {/* Only render if user is logged in as admin*/}
-            <Route exact path={"/login"} component={Login}/> 
-            <Route exact path={"/onboarding"} component={Onboarding}/>
-          </Switch>
-        </div>
-      </Router>
-    </AuthProvider>
+            <Route exact path={"/onboarding"} component={Onboarding} />
+          </> : null}
+          <Route path="/">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
