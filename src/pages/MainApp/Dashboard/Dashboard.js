7a0b1a1f84a793c15cb3a3f5e2ea48b255ef8389
ImJustChew <chewtzihwee@gmail.com>
2021-07-15 23:58:05 +0800

    added button and hidden compose button by default

--- src/pages/MainApp/Dashboard/Dashboard.js
+++ src/pages/MainApp/Dashboard/Dashboard.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MentorShowcase from '../../../components/Dashboard/MentorShowcase';
 import PostComposerCard from '../../../components/Dashboard/PostComposerCard';
 import PostStream from '../../../components/Dashboard/PostStream';
 import NotificationShowcase from '../../../components/Dashboard/NotificationShowcase';
+import EditIcon from '@material-ui/icons/Edit';
+import CancelIcon from '@material-ui/icons/Cancel';
+import { IconButton } from '@material-ui/core';
 
 import './Dashboard.css';
 
 function Dashboard() {
+    const [createPost, setCreatePost] = useState(false);
     return (
         <div className="dashboard">
             <MentorShowcase/>
             <div className="stream_container shadow-lg m-4">
-                <p className="font-semibold text-lg pl-4 pt-4">Your Feed</p>
-                <PostComposerCard />
+                <div className="flex flex-row justify-between w-full">
+                    <p className="font-semibold text-lg pl-4 pt-4">Your Feed</p>
+                    {!createPost ? <IconButton aria-label="delete" className="float-right" onClick={() => setCreatePost(true)}>
+                        <EditIcon className="text-blue-600"/>
+                    </IconButton>:<IconButton aria-label="delete" className="float-right" onClick={() => setCreatePost(false)}>
+                        <CancelIcon className="text-red-600"/>
+                    </IconButton>}
+                </div>
+                {createPost && <PostComposerCard />}
                 <PostStream/>
             </div>
             <div className="flex-1">
