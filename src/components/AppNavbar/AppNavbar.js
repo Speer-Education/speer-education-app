6843dc42063e1d8008d67701451d42d240765b99
ImJustChew <chewtzihwee@gmail.com>
2021-07-17 20:59:31 +0800

    usable navbar

--- src/components/AppNavbar/AppNavbar.js
+++ src/components/AppNavbar/AppNavbar.js
@@ -5,14 +5,17 @@ import MessageTwoToneIcon from '@material-ui/icons/MessageTwoTone';
 import NotificationsTwoToneIcon from '@material-ui/icons/NotificationsTwoTone';
 import ArrowDropDownTwoToneIcon from '@material-ui/icons/ArrowDropDownTwoTone';
 import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
-export const NavBarLink = ({IconComponent, title}) => {
-  return <div className="grid place-items-center h-full px-5 hover:bg-gray-100 cursor-pointer">
-    <div className="flex-1 flex flex-col items-center">
-      <IconComponent className="h-8 w-8"/>
-      <p className="">{title}</p>
+export const NavBarLink = ({IconComponent, title, href}) => {
+  return <Link to={href}>
+    <div className="grid place-items-center h-full px-5 hover:bg-gray-100 cursor-pointer">
+      <div className="flex-1 flex flex-col items-center">
+        <IconComponent className="h-8 w-8"/>
+        <p className="">{title}</p>
+      </div>
     </div>
-  </div>
+  </Link>
 }
 
 export const AppNavbar = () => {
@@ -20,9 +23,9 @@ export const AppNavbar = () => {
     <div className="flex flex-row w-full justify-evenly items-center p-8 h-32">
       <img className="h-20" src="/full-transparent-logo.png"/>
       <div className="flex flex-row h-full">
-        <NavBarLink IconComponent={HomeTwoToneIcon} title="Home"/>
-        <NavBarLink IconComponent={PeopleTwoToneIcon} title="My Mentors"/>
-        <NavBarLink IconComponent={MessageTwoToneIcon} title="Messaging"/>
+        <NavBarLink IconComponent={HomeTwoToneIcon} title="Home" href="/app"/>
+        <NavBarLink IconComponent={PeopleTwoToneIcon} title="My Mentors" href="/app/mentors"/>
+        <NavBarLink IconComponent={MessageTwoToneIcon} title="Messaging" href="/app/messages"/>
         <NavBarLink IconComponent={NotificationsTwoToneIcon} title="Notifications"/>
       </div>
       <Button variant="outlined" color="primary" endIcon={<ArrowDropDownTwoToneIcon/>}>
