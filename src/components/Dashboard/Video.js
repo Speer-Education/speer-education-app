b680ab9103373922af1c1602eaf99c1828eb6841
kheeong <42634274+kheeong@users.noreply.github.com>
2021-07-19 23:50:58 +0800

    V2

--- src/components/Dashboard/Video.js
+++ src/components/Dashboard/Video.js
@@ -1,14 +1,96 @@
-import React from "react";
 import PropTypes from "prop-types";
 import './Video.css';
-const YoutubeEmbed = ({ embedId }) => (
-  <div className="video-responsive">
-    <img src={`http://img.youtube.com/vi/${embedId}/0.jpg`} height="100%" width="100%"/>
-  </div>
-);
+import React, { useEffect, useState, Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+export default function YoutubeEmbed({ embedId }) {
+  let [isOpen, setIsOpen] = useState(true)
 
-YoutubeEmbed.propTypes = {
-  embedId: PropTypes.string.isRequired
-};
+  function closeModal() {
+    setIsOpen(false)
+  }
 
-export default YoutubeEmbed;
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  YoutubeEmbed.propTypes = {
+    embedId: PropTypes.string.isRequired
+  };
+  useEffect(() => {
+    console.log("overhere");
+    console.log(fetch("https://gdata.youtube.com/feeds/api/videos/" + embedId + "?v=2&alt=json-in-script&format=5&callback=getTitle").then(response => response.json()));
+  }, []);
+  return (
+    <>
+      <div className="flex flex-row w-300 min-w-500 p-3 m-2 shadow-lg rounded-md bg-white">
+        <img src={`http://img.youtube.com/vi/${embedId}/0.jpg`} height="90px" width="160px" onClick={openModal} />
+        <p>123</p>
+      </div> 
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-0"
+          onClose={closeModal}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-max max-w-full h-max max-h-full p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <div className="mt-2">
+                <iframe
+                  width="800px"
+                  height="450px"
+                  src={`https://www.youtube.com/embed/${embedId}`}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  title="Embedded youtube"
+                />
+                </div>
+
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                    onClick={closeModal}
+                  >
+                    Close Video
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
+
+
+
