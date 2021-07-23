490b67c01d059ea9ca0ecb4fd0a49f6a3a6aa4bd
Ee-Hsin <eehsinkok777@gmail.com>
2021-07-23 18:18:25 +0800

    Made mentorshowcase name link to profiles, and the button link to chat room

--- src/components/Dashboard/MentorShowcase.js
+++ src/components/Dashboard/MentorShowcase.js
@@ -1,17 +1,19 @@
 import { Button } from '@material-ui/core';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { db } from '../../config/firebase';
+import { db, functions } from '../../config/firebase';
 import ProfilePicture from '../User/ProfilePicture';
 import PersonAddTwoToneIcon from '@material-ui/icons/PersonAddTwoTone';
 import { IconButton } from '@material-ui/core';
 import './MentorShowcase.css';
 import { useAuth } from '../../hooks/useAuth';
 import { followUser } from '../../utils/relationships';
+import history from '../../hooks/history';
 
 export default function MentorShowcase() {
 
     const [mentors, setMentors] = useState([]);
+    const [creatingRoom, setCreatingRoom] = useState(false);
     const { user } = useAuth();
     
     //Loads the mentors in mentor collection
@@ -39,27 +41,41 @@ export default function MentorShowcase() {
         return array;
     }
 
-    const handleAddMentor = async (mentorId) => {
-        if(!user?.uid) return; //Check if user has loaded
-        await followUser(user.uid, mentorId)
-        console.log('followed user')
+    // const handleAddMentor = async (mentorId) => {
+    //     if(!user?.uid) return; //Check if user has loaded
+    //     await followUser(user.uid, mentorId)
+    //     console.log('followed user')
+    // }
+
+    const connectWithMentor = async (mentorId) => {
+        /* Send Mentor ID to backend for checking and room creation */
+
+        setCreatingRoom(true);
+
+        //send it in as profile id instead of mentor id (will need to change the backend so this still works)
+        let { data: targetRoomId } = await functions.httpsCallable('createRoom')({ profileId: mentorId })
+            .catch((error) => {
+                console.error(error)
+            })
+        history.push(`/app/messages/${targetRoomId}`)
     }
 
     return (
         <div className="mentorShowcase">
             <p>New Mentors To Find</p>
             {/* Randomly generates 2 mentors in random order*/}
-            {mentors.map(({ id, name, school, major, bio }) => <div className="flex flex-row p-2">
-                <ProfilePicture className="w-10 h-10 rounded-full" uid={id}/>
-                <div className="flex-1 ml-2">
-                    <h3>{name}</h3>
-                    <p>{major.label}</p>
-                </div>
-                <IconButton onClick={() => handleAddMentor(id)}>
+            {mentors.map(({ id, name, school, major, bio }) => <div className="flex flex-row p-2 ">
+                <Link className="flex flex-row flex-1" to={`/app/profile/${id}`}>
+                    <ProfilePicture className="w-10 h-10 rounded-full" uid={id}/>
+                    <div className="ml-2">
+                        <h3>{name}</h3>
+                        <p>{major.label}</p>
+                    </div>
+                </Link>
+                {creatingRoom? "Loading..." : 
+                <IconButton onClick={() => connectWithMentor(id)}>
                     <PersonAddTwoToneIcon/>
-                </IconButton>
-                
-                
+                </IconButton>}
             </div>)}
         </div>
     )
