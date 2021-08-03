08c755e37c78ece2bb170fdd122fbed16af416c6
ImJustChew <chewtzihwee@gmail.com>
2021-08-03 14:27:46 +0800

    Added tonnes of styling on onboarding page

--- src/pages/Onboarding/Onboarding.js
+++ src/pages/Onboarding/Onboarding.js
@@ -7,6 +7,51 @@ import { useAuth } from '../../hooks/useAuth';
 import { functions } from '../../config/firebase';
 import Button from '@material-ui/core/Button';
 import {Helmet} from "react-helmet";
+import Spinner from '../../components/Loader/Spinner';
+
+
+const InputField = ({ className, label, id, required = false, onChange, ...props }) => {
+    const [empty, setEmpty] = useState(false);
+
+    const handleInputChange = e => {
+        onChange(e)
+        if(required) setEmpty(e.target.value.length == 0)
+    }
+
+    return <div className={`w-full px-3 ${className}`}>
+        {label && <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for={id}>
+            {label} {required?<span className="text-red-600">*</span>:""}
+        </label>}
+        <input className={`appearance-none block w-full ${empty && required ?"bg-red-100":"bg-gray-200"} text-gray-700 border-0 focus:border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
+            id={id}
+            onChange={handleInputChange}
+            {...props}/>
+    </div>
+}
+
+const InputAreaField = ({ className, label, id, required = false, ...props }) => {
+    return <div className={`w-full px-3 ${className}`}>
+        {label && <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for={id}>
+            {label} {required?<span className="text-red-600">*</span>:""}
+        </label>}
+        <textarea className="appearance-none block w-full bg-gray-200 text-gray-700 border-0 focus:border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+            id={id}
+            {...props}/>
+    </div>
+}
+
+const InputSelect = ({ className, label, id, required = false, ...props }) => {
+    return <div className={`w-full px-3 ${className}`}>
+        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for={id}>
+            {label} {required?<span className="text-red-600">*</span>:""}
+        </label>
+        <Select className="user-details__custom-select" id={id} {...props}/>
+    </div>
+}
+
+const FormRow = ({ children }) => (
+  <div class="flex flex-wrap -mx-3 mb-6">{children}</div>
+)
 
 export default function UserDetails() {
     const { user } = useAuth();
@@ -19,7 +64,8 @@ export default function UserDetails() {
         school: "",
         major: "",
         bio: ""
-    });
+    }); 
+    const [updatingClaims, setUpdatingClaims] = useState(false);
     const [submitting, setSubmitting] = useState(false);
 
     // To check if all fields are filled up
@@ -49,11 +95,15 @@ export default function UserDetails() {
             console.log("Submission unsuccessful, please fill up all the inputs")
             setSubmitting(false);
         }
+        const submitForm = form;    
+        submitForm.country = form.country.value
+        submitForm.grade = form.grade.value
 
-        await functions.httpsCallable('onBoarding')({form})
+        await functions.httpsCallable('onBoarding')({form: submitForm})
             .catch((error) => {
                 console.error(error)
             })
+        setUpdatingClaims(true);
     }
 
     //Updates state whenever the user change fields in the form
@@ -72,53 +122,67 @@ export default function UserDetails() {
         setForm({...form, grade: value})
     }
 
-    const handleMajorSelectFormInput = value => {
-        setForm({...form, major: value})
-    }
-
     return (
-        <div className="user-details">
+        <div className="bg-gray-100 h-screen">
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>Sign Up | Speer Education</title>
             </Helmet>
-            <h1>Tell us about yourself!</h1>
-            <form className="user-details__form">
-                {/* Ask which to make mandatory and then force them to be required*/}
-                {/* Name */}
-                <label htmlFor="preferred-name">What's your Full Name? (Put first name before last name)</label>
-                <input type="text" id="preferred-name" placeholder="Name" name="name" value={form.name} onChange={handleFormInput}/>
-                {/* Grade */}
-                <label htmlFor="grade">What Grade Are You In?</label>
-                <Select className="user-details__custom-select" id="grade" name="grade" options={gradeOptions} value={form.grade} onChange={handleGradeSelectFormInput}/>
-                {/* Date Of Birth */}
-                <label htmlFor="dateOfBirth">What's you Date Of Birth?</label>
-                <input type="date" id="dateOfBirth" value={form.date} max="2012-12-31" name="dateOfBirth" onChange={handleFormInput} />
-                {/* Insert Country They Live In (Use a country list)*/}
-                <label htmlFor="country">Country Of Residence</label>
-                <Select className="user-details__custom-select" id="country" name="country" options={countryOptions} value={form.country} onChange={handleCountrySelectFormInput} />
-                {/* Their school Name: */}
-                <label htmlFor="school">What's the name of your school?</label>
-                <input type="text" id="school" placeholder="school" name="school" value={form.school} onChange={handleFormInput}/>
-                {/* What they plan to major in */}
-                <label htmlFor="major">What Do You Plan To Major In?</label>
-                <Select className="user-details__custom-select" id="major" name="major" options={majorOptions} value={form.major} onChange={handleMajorSelectFormInput}/>
-                {/* Interests/hobbies */}
-                <label htmlFor="bio">Give a short 50 word bio about yourself, your aspirations or hobbies etc...</label>
-                <input name="bio" placeholder="Short Bio (50 words)" onChange={handleFormInput} value={form.bio}></input>
-                {!isValidForm && <p className="text-red-600">Error! Please fill in all the blanks.</p>}
-                {/* Submission Button */}
-                <Button
-                    type="submit"
-                    onClick={handleFormSubmit}
-                    disabled={!isValidForm || submitting}
-                    variant="outlined"
-                >
-                    {submitting?"Submitting...":"Submit"}
-                </Button>
+            {!updatingClaims ? <div className="flex flex-row w-screen h-screen">
+                <div className="flex-1 text-left p-12">
+                    <h1>Tell us about yourself!</h1>
+                    <div className="flex flex-col">
+                        {/* Ask which to make mandatory and then force them to be required*/}
+                        <div className="mt-2">
+                            <FormRow>
+                                {/* Name */}
+                                <InputField required type="text" className="md:w-full mb-6 md:mb-0" label="Full Name"  placeholder="John Doe" id="fullname" name="name" value={form.name} onChange={handleFormInput} />
+                            </FormRow>
+                            <FormRow>                            
+                                {/* Date Of Birth */}
+                                <InputField required type="date" className="md:w-1/2 mb-6 md:mb-0" label="Date of Birth" id="dateofbirth" name="dateOfBirth" value={form.dateOfBirth} onChange={handleFormInput} />
+                                {/* Insert Country They Live In (Use a country list)*/}
+                                <InputSelect required className="md:w-1/2 mb-6 md:mb-0" label="Country" id="country" name="country" options={countryOptions} value={form.country} onChange={handleCountrySelectFormInput} />
+                            </FormRow>
+                            <FormRow>
+                                {/* Their school Name: */}
+                                <InputField required type="text" className="md:w-1/3 mb-6 md:mb-0" label="Current Education Institute" id="school" name="school" placeholder="University Malaya" value={form.school} onChange={handleFormInput} />
+                                {/* Grade */}
+                                <InputSelect required className="md:w-1/3 mb-6 md:mb-0" label="Grade" id="grade" name="grade" options={gradeOptions} value={form.grade} onChange={handleGradeSelectFormInput}/>
+                                {/* What they plan to major in */}
+                                <InputField required type="text" className="md:w-1/3 mb-6 md:mb-0" label="Your Major" placeholder="Economics, Business ...." id="major" name="major" value={form.major} onChange={handleFormInput}/>
+                            </FormRow>
+                            <FormRow>
+                                <InputAreaField type="text" label="Short Biography" id="bio" name="bio" placeholder="Tell us something about yourself........" value={form.bio} onChange={handleFormInput} />
+                            </FormRow>
+                        </div>
+                        {/* Interests/hobbies */}
+                        {!isValidForm && <p className="text-red-600">Error! Please fill in all the blanks.</p>}
+                        {/* Submission Button */}
+                        <Button
+                            type="submit"
+                            onClick={handleFormSubmit}
+                            disabled={!isValidForm || submitting}
+                            variant="outlined"
+                        >
+                            {submitting?<Spinner/>:"Submit"}
+                        </Button>
 
-                {/* Change the other selects to the cool select form */}
-            </form>
+                        {/* Change the other selects to the cool select form */}
+                    </div>
+                </div>
+                <div className="flex-1 bg-white">
+                    <img className="w-full h-full object-contain p-32" src="./rocket-logo@3x.png"/>
+                </div>
+            </div>: <div className="grid place-items-center w-screen h-screen">
+                <div className="flex flex-col items-center space-y-2">
+                    <img className="object-contain w-24 p-4" src="./rocket-logo@3x.png"/>
+                    <h2>Setting Up Your Account...</h2>
+                    <p>Please Wait</p>
+                    <div className="animate-pulse h-2 rounded-md w-52 bg-gray-400"></div>
+                </div>
+            </div>}
+            
         </div>
     )
 }
