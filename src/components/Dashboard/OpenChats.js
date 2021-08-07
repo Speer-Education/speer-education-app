df2d628dd67e7586cec655198f758bbca32ac772
Ee-Hsin <eehsinkok777@gmail.com>
2021-08-07 10:19:52 +0800

    Fixed overflow for recent chats and made cursor pointer for like and comment buttons

--- src/components/Dashboard/OpenChats.js
+++ src/components/Dashboard/OpenChats.js
@@ -27,28 +27,29 @@ export default function OpenChats() {
                 }
             })
             //Order Active chats by chronological order
-            setChatrooms(rooms.sort(({date: x},{date: y}) => x.toMillis() - y.toMillis()))
+            setChatrooms(rooms.sort(({ date: x }, { date: y }) => x.toMillis() - y.toMillis()))
         }
     }, [userDetails?.activeRooms])
 
     return (
-        <div className="flex flex-col rounded-md bg-white m-2 w-300 min-w-500 shadow-md flex-1" style={{'height': '300px'}}>
+        <div className="flex flex-col rounded-md bg-white m-2 w-300 min-w-500 shadow-md flex-1 " style={{ 'height': '300px' }}>
             {!chatrooms ? <div className="h-full p-3 grid place-items-center">
                 {/* TOOD: Add No Recent Chats Icon */}
                 <h2 className="text-gray-500">No Recent Chats</h2>
             </div> : <>
-            <p className="p-3">Recent Chats</p>
-            {chatrooms.map(({senderUsername, senderId, message, roomId})=> (<Link to={`/app/messages/${roomId}`}>
-                    <div className="flex flex-row hover:bg-gray-100 cursor-pointer rounded-xl px-3 py-1">
-                        <ProfilePicture uid={senderId} className="w-10 h-10 rounded-full"/>
-                        <div className="flex-1 ml-2 max-w-full">
-                            <h3 className="font-medium">{senderUsername}</h3>
-                            <p className="overflow-hidden overflow-ellipsis whitespace-nowrap w-10/12 text-gray-500">{message}</p>
+                <p className="p-3">Recent Chats</p>
+                <div className="overflow-hidden">
+                    {chatrooms.map(({ senderUsername, senderId, message, roomId }) => (<Link to={`/app/messages/${roomId}`}>
+                        <div className="flex flex-row hover:bg-gray-100 cursor-pointer rounded-xl px-3 py-1 ">
+                            <ProfilePicture uid={senderId} className="w-10 h-10 rounded-full" />
+                            <div className="flex-1 ml-2 max-w-full">
+                                <h3 className="font-medium">{senderUsername}</h3>
+                                <p className="overflow-hidden overflow-ellipsis whitespace-nowrap w-10/12 text-gray-500">{message}</p>
+                            </div>
                         </div>
-                    </div>
-                </Link>
-            ))}
-            
+                    </Link>
+                    ))}
+                </div>
             </>}
             <div className="mt-auto p-3"><Link to="/app/messages" className="text-blue-700 underline text-xs">See all Chats</Link></div>
         </div>
