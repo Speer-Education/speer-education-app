513100ceadfece1bb0bc69d13b8f4911cce40ed5
Ee-Hsin <eehsinkok777@gmail.com>
2021-08-10 11:37:38 +0800

    Added firebase analytics

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,6 +4,7 @@ import "firebase/firestore";
 import "firebase/database";
 import "firebase/storage";
 import "firebase/functions";
+import "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAx-OEKEZF6LgX5wv03qRilbGTWIJvL4kw",
@@ -13,6 +14,7 @@ const firebaseConfig = {
   storageBucket: "speer-education-dev.appspot.com",
   messagingSenderId: "768215592962",
   appId: "1:768215592962:web:944d85b44e2cdc2e0c371b",
+  measurementId: "G-19MWDB3T0C"
 };
 
 if (!firebase.apps.length) {
@@ -31,8 +33,17 @@ const now = firebase.firestore.Timestamp.now();
 const storage = firebase.storage();
 const functions = firebase.functions();
 
+let analytics = {};
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  // dev code
+} else {
+  // production code
+  analytics = firebase.analytics();
+}
+
 db.enablePersistence()
 
-export { firebase, auth, db, now, storage, rtdb, functions };
+export { firebase, auth, db, now, storage, rtdb, functions, analytics };
 
 console.log(app.name ? "Firebase Mode Activated!" : "Firebase not working :(");
