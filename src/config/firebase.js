235a7791d961cc6c578a8fb405114c8feb7180aa
ImJustChew <chewtzihwee@gmail.com>
2021-08-10 12:01:17 +0800

    Revert "Added firebase analytics"

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,7 +4,6 @@ import "firebase/firestore";
 import "firebase/database";
 import "firebase/storage";
 import "firebase/functions";
-import "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAx-OEKEZF6LgX5wv03qRilbGTWIJvL4kw",
@@ -14,7 +13,6 @@ const firebaseConfig = {
   storageBucket: "speer-education-dev.appspot.com",
   messagingSenderId: "768215592962",
   appId: "1:768215592962:web:944d85b44e2cdc2e0c371b",
-  measurementId: "G-19MWDB3T0C"
 };
 
 //Make sure there are firebase apps loaded to initialize
@@ -35,23 +33,16 @@ const now = firebase.firestore.Timestamp.now();
 const storage = firebase.storage();
 const functions = firebase.functions();
 
-let analytics = {};
-
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  // dev code
-} else {
-  // production code
-  analytics = firebase.analytics();
-}
-
 // FOR LOCAL EMULATORS  =================
 // db.useEmulator("localhost", 8080);
 // rtdb.useEmulator("localhost", 9000);
 // functions.useEmulator("localhost", 5001);
 
+
 //Enable persistence for firestore so it saves in browser
 db.enablePersistence()
 
-export { firebase, auth, db, now, storage, rtdb, functions, analytics };
+
+export { firebase, auth, db, now, storage, rtdb, functions };
 
 console.log(app.name ? "Firebase Mode Activated!" : "Firebase not working :(");
