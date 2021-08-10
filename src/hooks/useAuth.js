57b2f7f200f53d65a98017ebf3e5661d3994cbd9
ImJustChew <chewtzihwee@gmail.com>
2021-08-10 23:06:46 +0800

    fixed react not updating user Details

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -28,6 +28,10 @@ export const useAuth = () => {
  * Logic required to run the auth methods for Firebase
  * @returns { user, userDetails, signInWithEmailAndPassword, signOut, initGoogleSignIn, initFacebookSignIn }
  */
+
+//Use variable as userDetails might be immediately needed before react even renders
+let latestUserDetails = {}
+
 const useAuthProvider = () => {
     const [user, setUser] = useState(null); 
     const [userDetails, setUserDetails] = useState(null);
@@ -122,7 +126,7 @@ const useAuthProvider = () => {
                 if(!data?._lastCommitted) return;
 
                 if (lastCommitted && !(data?._lastCommitted || {}).isEqual(lastCommitted)) {
-                    setUserDetails({ ...await getUserTokenResult(true), ...userDetails })
+                    setUserDetails({ ...await getUserTokenResult(true), ...latestUserDetails })
                     console.log("Refreshing token");
                 }
                 console.log('User Claims Updated', data);
@@ -139,7 +143,8 @@ const useAuthProvider = () => {
                 .collection("users")
                 .doc(user.uid)
                 .onSnapshot(async (doc) => {
-                    setUserDetails({ ...await getUserTokenResult(), ...doc.data() })
+                    latestUserDetails = { ...await getUserTokenResult(), ...doc.data() }
+                    setUserDetails(latestUserDetails)
                     console.log('User Document Updated')
                 });
             return () => unsubscribe();
