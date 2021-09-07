1928fc9cf8492222fc634721ba1e84dc05f7437e
ImJustChew <chewtzihwee@gmail.com>
2021-09-07 20:23:23 +0800

    profile on navbar styling

--- src/components/AppNavbar/UserMenu/UserMenu.js
+++ src/components/AppNavbar/UserMenu/UserMenu.js
@@ -7,9 +7,9 @@ const UserMenu = () => {
     if (!user) return <div></div>;
     return <div className="relative" >
         <div className="flex flex-col px-3 cursor-pointer" onClick={() => history.push('/app/profile')}>
-            <img className="w-6 h-6 lg:w-10 lg:h-10 overflow-hidden shadow-xl object-cover " src={user.photoURL} alt="avatar" />
+            <img className="w-6 h-6 lg:w-10 lg:h-10 overflow-hidden shadow-xl object-cover rounded-full" src={user.photoURL} alt="avatar" />
             <button className="border-none bg-transparent cursor-pointer">
-                <span className="pr-1 lg:text-base text-xs">Me</span><i className="fas fa-caret-down" style={{color: "#F58A07"}}></i>
+                <span className="lg:text-base text-xs underline text-speer-blue">Me</span>
             </button>
         </div>
     </div>
