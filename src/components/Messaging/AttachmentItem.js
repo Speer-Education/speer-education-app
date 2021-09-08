04fc9152e607b5213ff97b3a1d65f8b2cb7902bc
ImJustChew <chewtzihwee@gmail.com>
2021-09-08 19:54:58 +0800

    fixed paragraph nesting error

--- src/components/Messaging/AttachmentItem.js
+++ src/components/Messaging/AttachmentItem.js
@@ -1,14 +1,14 @@
 import TimeAgo from 'react-timeago';
 
 const AttachmentItem = ({ image, IconComponent, title, hoverClass="hover:bg-gray-100", subtitle,date, onClick }) => (
-    <div className={`flex flex-row space-x-2 py-1 rounded-lg cursor-pointer ${hoverClass} w-full`} onClick={onClick}>
+    <attachment className={`flex flex-row space-x-2 py-1 rounded-lg cursor-pointer ${hoverClass} w-full`} onClick={onClick}>
         <div className="h-12 w-12 bg-red-200 grid place-items-center rounded-xl overflow-hidden">
             {image?<img className="h-full w-full object-cover" src={image}/>:<IconComponent />}
         </div>
         <div className="flex flex-col space-y-1 flex-1 overflow-hidden">
             <h4 className="overflow-hidden overflow-ellipsis whitespace-nowrap w-10/12">{title}</h4>
-            <p className="overflow-hidden overflow-ellipsis whitespace-nowrap w-10/12 text-gray-500"><TimeAgo date={date} />{subtitle}</p>
+            <span className="overflow-hidden overflow-ellipsis whitespace-nowrap w-10/12 text-gray-500"><TimeAgo date={date} />{subtitle}</span>
         </div>
-    </div>)
+    </attachment>)
 
 export default AttachmentItem
