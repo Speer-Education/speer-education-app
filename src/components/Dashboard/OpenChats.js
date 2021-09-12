59d33f7f703ebe98b95cb882d5033e24ebab0b51
ImJustChew <chewtzihwee@gmail.com>
2021-09-12 17:29:20 +0800

    refactored code

--- src/components/Dashboard/OpenChats.js
+++ src/components/Dashboard/OpenChats.js
@@ -4,6 +4,24 @@ import ProfilePicture from '../User/ProfilePicture';
 import { Link } from 'react-router-dom';
 import ReactTimeago from 'react-timeago';
 import { db } from '../../config/firebase';
+import { Button } from '@material-ui/core';
+import history from '../../hooks/history';
+
+
+const ActiveChatRow = ({ photoUid, username, message, date, isUnread, roomId}) => (
+    <Link to={`/app/messages/${roomId}`} key={roomId}>
+        <div className="flex flex-row hover:bg-gray-100 cursor-pointer rounded-xl px-3 py-1 ">
+            <ProfilePicture uid={photoUid} thumb className="w-10 h-10 rounded-full" />
+            <div className="flex-1 ml-2 max-w-full">
+                <h3 className="font-medium">{username}</h3>
+                <div className="w-full flex flex-row text-gray-500 text-sm">
+                    <p className="overflow-hidden overflow-ellipsis whitespace-nowrap flex-1">{message}</p>
+                    {date && <ReactTimeago className="text-gray-400" date={date.toMillis()} />}
+                </div>
+            </div>
+        </div>
+    </Link>
+)
 
 export default function OpenChats() {
 
@@ -33,67 +51,37 @@ export default function OpenChats() {
         <div className="flex flex-col rounded-md bg-white m-2 w-300 min-w-500 shadow-md flex-1 " style={{ 'height': '300px' }}>
             {!chatrooms ? <div className="h-full p-3 grid place-items-center">
                 {/* TOOD: Add No Recent Chats Icon */}
-                <h2 className="text-gray-500">No Recent Chats</h2>
+                <div className="space-y-2 grid place-items-center">
+                    <h2 className="text-gray-500">No Recent Chats</h2>
+                    <Button variant="contained" color="primary" onClick={() => history.push('/app/mentors')}>Find a mentor</Button>
+                </div>
             </div> : <>
                 <p className="p-3">Recent Chats</p>
                 <div className="overflow-hidden">
-                    {chatrooms.map(({ senderUsername, senderId, recipientIds, message, roomId, date, roomName }) => {
+                    {chatrooms.map(({ senderUsername, senderId, recipientIds, message, roomId, date, roomName, read }) => {
                     
-                    console.log("recipient Ids:", recipientIds);
                     //For non group chats
                     if (recipientIds?.length === 1) {
-                        
-                        //means you are the sender and only 2 people in chat room, we can use the recipient's id for the profile picture
-                        if (senderId === user?.uid){
-
-                            return <Link to={`/app/messages/${roomId}`} key={roomId}>
-                                <div className="flex flex-row hover:bg-gray-100 cursor-pointer rounded-xl px-3 py-1 ">
-                                    <ProfilePicture uid={recipientIds[0]} thumb className="w-10 h-10 rounded-full" />
-                                    <div className="flex-1 ml-2 max-w-full">
-                                        <h3 className="font-medium">{roomName[user?.uid] || "Feature in progress..."}</h3>
-                                        <div className="w-full flex flex-row text-gray-500 text-sm">
-                                            <p className="overflow-hidden overflow-ellipsis whitespace-nowrap flex-1">{senderId === user?.uid?"You: ":""}{message}</p>
-                                            {date && <ReactTimeago className="text-gray-400" date={date.toMillis()} />}
-                                        </div>
-                                    </div>
-                                </div>
-                            </Link>
-                        //Means other person is sender
-                        } else {
-
-                            return <Link to={`/app/messages/${roomId}`} key={roomId}>
-                                <div className="flex flex-row hover:bg-gray-100 cursor-pointer rounded-xl px-3 py-1 ">
-                                    <ProfilePicture uid={senderId} thumb className="w-10 h-10 rounded-full" />
-                                    <div className="flex-1 ml-2 max-w-full">
-                                        <h3 className="font-medium">{senderUsername}</h3>
-                                        <div className="w-full flex flex-row text-gray-500 text-sm">
-                                            <p className="overflow-hidden overflow-ellipsis whitespace-nowrap flex-1">{senderId === user?.uid?"You: ":""}{message}</p>
-                                            {date && <ReactTimeago className="text-gray-400" date={date.toMillis()} />}
-                                        </div>
-                                    </div>
-                                </div>
-                            </Link>
-                        }
+                        return <ActiveChatRow 
+                            key={roomId} 
+                            photoUid={(senderId === user?.uid) ? recipientIds[0] : senderId} 
+                            username={(senderId === user?.uid) ? (roomName[user?.uid] || "Unread Message"):senderUsername} 
+                            message={`${senderId === user?.uid ? "You: ":"" }${message}`} 
+                            date={date} 
+                            isUnread={read?.[user.uid]}
+                            roomId={roomId} />
                     }
                     
                     //Means it is a group chat
                     if (recipientIds?.length > 1){
-                        return <Link to={`/app/messages/${roomId}`} key={roomId}>
-                            <div className="flex flex-row hover:bg-gray-100 cursor-pointer rounded-xl px-3 py-1 ">
-                                <ProfilePicture uid={roomId} isRoom thumb className="w-10 h-10 rounded-full" />
-                                <div className="flex-1 ml-2 max-w-full">
-                                    {/* TODO: This should switch to roomName */}
-                                    <h3 className="font-medium">{roomName[user?.uid]}</h3>
-                                    <div className="w-full flex flex-row text-gray-500 text-sm">
-                                        {/* TODO: This should switch to the sender's username instead of just "You" */}
-                                        <p className="overflow-hidden overflow-ellipsis whitespace-nowrap flex-1">{senderId === user?.uid?"You: ":`${senderUsername}:`}{message}</p>
-                                        {date && <ReactTimeago className="text-gray-400" date={date.toMillis()} />}
-                                    </div>
-                                </div>
-                            </div>
-                        </Link>
-                    } else {
-                        return <></>
+                        return <ActiveChatRow 
+                            key={roomId} 
+                            photoUid={roomId} 
+                            username={roomName[user?.uid]} 
+                            message={`${senderId === user?.uid ? "You: ":`${senderUsername}: ` }${message}`} 
+                            date={date} 
+                            isUnread={read?.[user.uid]}
+                            roomId={roomId} />
                     }
                 })}
                 </div>
