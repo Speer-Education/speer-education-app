a525404b48c460374410cc80cb56b97027f149a1
ImJustChew <chewtzihwee@gmail.com>
2021-09-15 23:17:50 +0800

    added ring around user profile to make obvious your on profile page

--- src/components/AppNavbar/UserMenu/UserMenu.js
+++ src/components/AppNavbar/UserMenu/UserMenu.js
@@ -7,7 +7,10 @@ const UserMenu = () => {
     if (!user) return <div></div>;
     return <div className="relative" >
         <div className="flex flex-col px-3 cursor-pointer" onClick={() => history.push('/app/profile')}>
-            <img className="w-6 h-6 lg:w-10 lg:h-10 overflow-hidden shadow-xl object-cover rounded-full" src={`${user.photoURL}?${Date.now()}`} alt="avatar" />
+            <img 
+                className={`w-6 h-6 lg:w-10 lg:h-10 overflow-hidden transition-all duration-150 shadow-lg hover:shadow-xl object-cover rounded-full  ${('/app/profile' === history.location.pathname) ? 'border-2 border-solid border-[#F58A07]' : ''}`} 
+                src={`${user.photoURL}?${Date.now()}`} 
+                alt="avatar" />
         </div>
     </div>
 }
