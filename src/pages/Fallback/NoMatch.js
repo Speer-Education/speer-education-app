902adcd31e43ad2e43cf510a581177d7c4fc09fd
Ee-Hsin <eehsinkok777@gmail.com>
2021-09-23 22:25:52 +0800

    commented out unneeded providerHOC component

--- src/pages/Fallback/NoMatch.js
+++ src/pages/Fallback/NoMatch.js
@@ -4,15 +4,15 @@ const NoMatch = (props) => {
     return <Redirect to={{state: {noMatch: true}}} />
 }
 
-const ProviderHOC = (NotFoundRoute) => {
-    const RouteProvider = (props) => {
-        if(props.location && props.location.state && props.location.noMatch) {
-           return  <NotFoundRoute {...props} />
-        }
-        return props.children;
-    }
-    return withRouter(RouteProvider)
+// const ProviderHOC = (NotFoundRoute) => {
+//     const RouteProvider = (props) => {
+//         if(props.location && props.location.state && props.location.noMatch) {
+//            return  <NotFoundRoute {...props} />
+//         }
+//         return props.children;
+//     }
+//     return withRouter(RouteProvider)
 
-}
+// }
 
-export { ProviderHOC, NoMatch };
+export { NoMatch };
