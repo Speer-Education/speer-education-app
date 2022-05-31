07f932654fa6463f2a613d86fd15fa96764ff32f
ImJustChew <chewtzihwee@gmail.com>
2022-05-31 09:48:07 +0800

    Added extentions to mdeditor and fixed search

--- src/components/AppNavbar/SearchBar.tsx
+++ src/components/AppNavbar/SearchBar.tsx
@@ -1,100 +1,77 @@
-//@ts-nocheck
-import searchClient from '../../config/algolia';
-import {
-  InstantSearch,
-  connectSearchBox,
-  connectHits,
-  Configure
-} from 'react-instantsearch-dom';
 import ProfilePicture from '../User/ProfilePicture';
 import { SearchOutlined } from '@mui/icons-material';
-import useOnClickOutside from '../../hooks/useOnClickOutside';
-import React, { forwardRef, ForwardRefExoticComponent, useRef } from 'react';
+import React, { Dispatch, forwardRef, ForwardRefExoticComponent, SetStateAction } from 'react';
 import { logEvent } from '../../utils/analytics';
 import { Collapse } from '@mui/material';
 import { TransitionGroup } from "react-transition-group";
 import { useNavigate } from 'react-router-dom';
+import { usersIndex } from '../../config/algolia';
+import { FixMeLater } from '../../types/temp';
 
+let setSearchQuery: Dispatch<SetStateAction<string>> | null = null;
 
-const userSearchClient = {
-  ...searchClient,
-  search(requests) {
-    if (requests.every(({ params }) => !params.query)) {
-      return Promise.resolve({
-        results: requests.map(() => ({
-          hits: [],
-          nbHits: 0,
-          nbPages: 0,
-          page: 0,
-          processingTimeMS: 0,
-        })),
-      });
-    }
-
-    return searchClient.search(requests);
-  },
-}
-
-let setSearchQuery = null;
-
-const Hit: ForwardRefExoticComponent<any, any> = forwardRef(({ hit }, ref) => {
+const Hit = ({ hit }) => {
   const navigate = useNavigate();
-  return <div ref={ref} className="flex flex-row space-x-2 hover:bg-blue-500 hover:bg-opacity-10 px-4 py-2 cursor-pointer" onClick={() => {
+  return <div className="flex flex-row space-x-2 hover:bg-blue-500 hover:bg-opacity-10 px-4 py-2 cursor-pointer" onClick={() => {
     navigate(`/profile/${hit.objectID}`)
     logEvent("clicked_on_search", {
       targetUser: hit.objectID,
     });
-    setSearchQuery("")
+    if(setSearchQuery) setSearchQuery("")
   }}>
-    <ProfilePicture className="h-10 w-10 rounded-full" uid={hit.objectID} thumb/>
+    <ProfilePicture className="h-10 w-10 rounded-full" uid={hit.objectID} thumb />
     <div className="flex-col">
       <h3 className="font-semibold">{hit.name}</h3>
       <p className="text-gray-500 text-sm">{hit.major}</p>
     </div>
   </div>
-});
+};
 
 const Hits = ({ hits }) => (
   <div className="hidden lg:flex absolute flex-col mt-2 shadow-xl bg-white rounded-xl overflow-hidden min-w-[400px] max-h-[60vh] overflow-y-auto scrollbar-hide">
     <TransitionGroup>
       {hits.map(hit => (
         <Collapse in key={hit.objectID}>
-          <Hit hit={hit}/>
+          <Hit hit={hit} />
         </Collapse>
       ))}
     </TransitionGroup>
   </div>
 );
-
-const SearchBox = ({ currentRefinement, refine }) => {
-  setSearchQuery = refine;
-  return <form noValidate action="" role="search">
-    <div className="hidden lg:flex flex-row items-center w-80 bg-gray-200 text-gray-700  py-3 px-4 rounded focus:bg-gray-300 focus:border-gray-500">
-      <SearchOutlined className="text-gray-500 w-8 h-8" />
-      <input className="appearance-none ml-1 flex-1 border-0 leading-tight focus:outline-none bg-gray-200 ring-0 focus:border-0"
-        type="input"
-        id="search-box"
-        autoComplete="off"
-        placeholder="Search For Users"
-        value={currentRefinement}
-        onChange={event => refine(event.currentTarget.value)} />
-    </div>
-  </form>
-};
-
-const CustomHits = connectHits(Hits);
-const CustomSearchBox = connectSearchBox(SearchBox);
 const SearchBar = () => {
-  const searchRef = useRef();
-  useOnClickOutside(searchRef, () => setSearchQuery("")); 
+  const [hits, setHits] = React.useState<FixMeLater[]>([]);
+  const [search, setSearch] = React.useState<string | null>(null);
 
-  return <InstantSearch indexName="speer_users" searchClient={userSearchClient} >
-    <div className="relative" ref={searchRef}>
-      <CustomSearchBox/>
-      <Configure hitsPerPage={6} />
-      <CustomHits hitComponent={Hit} />
-    </div>
-  </InstantSearch>
+  setSearchQuery = setSearch;
+
+  React.useEffect(() => {
+    if(!search) setHits([]);
+    else {
+      usersIndex.search<{ name: string }>(search, {
+        hitsPerPage: 10
+      }).then(function (content) {
+        setHits(content.hits)
+      });
+    }
+  }, [search]);
+
+  return (<div className="relative">
+      <form noValidate action="" role="search">
+        <div className="hidden lg:flex flex-row items-center w-80 bg-gray-200 text-gray-700  py-3 px-4 rounded focus:bg-gray-300 focus:border-gray-500">
+          <SearchOutlined className="text-gray-500 w-8 h-8" />
+          <input className="appearance-none ml-1 flex-1 border-0 leading-tight focus:outline-none bg-gray-200 ring-0 focus:border-0"
+            type="input"
+            id="search-box"
+            autoComplete="off"
+            placeholder="Search For Users"
+            value={search || ""}
+            onChange={event => setSearch(event.currentTarget.value)} 
+            onFocus={() => setSearch("")}
+            onBlur={() => setSearch(null)}/>
+        </div>
+      </form>
+      <Hits hits={hits} />
+    </div>)
 }
 
 export default SearchBar
--- src/config/algolia.ts
+++ src/config/algolia.ts
@@ -1,8 +1,12 @@
 import algoliasearch from 'algoliasearch/lite';
 
+export const ALGOLIA_APP_ID = 'PTO0LGFSZW';
+export const ALGOLIA_API_KEY = '405959adb3a03a61241ccb3e68726a40'
 const searchClient = algoliasearch(
     'PTO0LGFSZW',
     '405959adb3a03a61241ccb3e68726a40'
   );
+
+export const usersIndex = searchClient.initIndex('speer_users');
 
 export default searchClient;
