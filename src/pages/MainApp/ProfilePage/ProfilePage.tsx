b6c39c69e1ee3a1703c7f2952eeb1650fafc823e
ImJustChew <chewtzihwee@gmail.com>
2022-06-21 18:34:50 +0800

    Check firestore ref in account of query fix Pagination Post Stream is not updating when changing profiles #37

--- src/pages/MainApp/ProfilePage/ProfilePage.tsx
+++ src/pages/MainApp/ProfilePage/ProfilePage.tsx
@@ -79,7 +79,7 @@ function ProfilePage({ isUser=false }: { isUser?: boolean }) {
                 <div className="w-screen h-full flex flex-row">
                     <div className="hidden xl:flex flex-col h-full h-app w-sidebar">
                         <div className="fixed flex flex-col cc_cursor h-app w-sidebar">
-                            {profileId || user?.uid && userDetails && <ContactsSidebar profileId={profileId || user?.uid} userDetails={userDetails} isUser={isUser}/>}
+                            {((profileId || user?.uid) && userDetails) && <ContactsSidebar profileId={profileId || user!.uid} userDetails={userDetails} isUser={isUser}/>}
                             <StatsCard />
                         </div>
                     </div>
