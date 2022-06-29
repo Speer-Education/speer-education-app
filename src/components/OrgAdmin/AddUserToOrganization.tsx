0d3ad3b99b69aa76001abc5aeefc6feb8e8d2be8
ImJustChew <chewtzihwee@gmail.com>
2022-06-30 00:54:10 +0800

    added user management

---
+++ src/components/OrgAdmin/AddUserToOrganization.tsx
@@ -0,0 +1,63 @@
+import { FC } from "react";
+import {Button, DialogActions, DialogContent, DialogTitle} from '@mui/material';
+import {useForm} from 'react-hook-form';
+import {useAuth} from '../../hooks/useAuth';
+import {UserHit} from '../../types/Algolia';
+import {httpsCallable} from 'firebase/functions';
+import {functions} from '../../config/firebase';
+import {UserID} from '../../types/User';
+import {useSnackbar} from 'notistack';
+import {useNavigate} from 'react-router-dom';
+import FormUserSelector from '../Forms/FormUserSelector';
+import { useSpeerOrg } from "../../hooks/useSpeerOrg";
+
+type FormValues = {
+    users: UserHit[];
+}
+
+const AddUserToOrganization: FC<{ onClose: () => void }> = ({ onClose }) => {
+    const { user } = useAuth();
+    const { orgId } = useSpeerOrg();
+    const { control, handleSubmit, watch, formState: { isValid, isSubmitting } } = useForm<FormValues>({
+        defaultValues: {
+            users: [],
+        },
+        mode: "onChange",
+    });
+    const { enqueueSnackbar } = useSnackbar();
+
+    const onSubmit = async (data: FormValues) => {
+        try {
+            console.log("TODO: add AddUserToOrganization backend", data)
+            // const UpdateGroupChat = httpsCallable<{ users: UserID[], orgId: string }, boolean>(functions, 'AddUserToOrganization');
+            // await UpdateGroupChat({
+            //     users: data.users.map(participant => participant.objectID),
+            //     orgId
+            // })
+            onClose();
+        } catch (e) {
+            console.error(e)
+            enqueueSnackbar(e.message, { variant: "error" })
+        }
+    }
+
+    return <>
+        <DialogTitle>Add Participants</DialogTitle>
+        <DialogContent sx={{minWidth: '260px'}}>
+            <h3>Search for users to add to your organization.</h3>
+            <p>Users in another organization <b>can't</b> join a new organization unless they quit their existing ones.</p>
+            <FormUserSelector 
+                control={control} 
+                name="users" 
+                label="Add Users" 
+                filter={(hit: UserHit) => (hit.objectID != user?.uid)}
+            />
+        </DialogContent>
+        <DialogActions>
+            <Button onClick={onClose}>Cancel</Button>
+            <Button onClick={handleSubmit(onSubmit)} disabled={!isValid || isSubmitting}>Add</Button>
+        </DialogActions>
+    </>
+}
+
+export default AddUserToOrganization;
---
+++ src/components/OrgAdmin/UsersManager.tsx
@@ -0,0 +1,120 @@
+import { query, where } from "firebase/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { functions, publicUserCollection } from "../../config/firebase";
+import { useSpeerOrg } from "../../hooks/useSpeerOrg";
+import { DataGrid, GridActionsCellItem, GridColDef, GridColumns, GridRowParams, GridToolbar, GridToolbarColumnsButton, GridToolbarContainer, GridToolbarDensitySelector, GridToolbarExport, GridToolbarFilterButton, GridValueGetterParams } from '@mui/x-data-grid';
+import { Button, DialogActions, DialogContent, DialogTitle } from "@mui/material";
+import { AddRounded, Delete, SecurityOutlined } from "@mui/icons-material";
+import { PublicUser, PublicUserDoc } from "../../types/User";
+import { useDialog } from "../../hooks/useDialog";
+import AddUserToOrganization from "./AddUserToOrganization";
+import { httpsCallable } from "firebase/functions";
+import { useSnackbar } from "notistack";
+
+function CustomToolbar() {
+    const [openDialog, closeDialog] = useDialog();
+
+    const showAddUserDialog = () => {
+        openDialog({
+            children: <AddUserToOrganization onClose={closeDialog} />
+        })
+    }
+    return (
+        <GridToolbarContainer>
+            <div className="flex flex-row w-full">
+                <div className="flex flex-row space-x-1 flex-1">
+                    <GridToolbarColumnsButton />
+                    <GridToolbarFilterButton />
+                    <GridToolbarDensitySelector />
+                    <GridToolbarExport />
+                </div>
+                <Button variant="contained" startIcon={<AddRounded />} onClick={showAddUserDialog}>Add User</Button>
+            </div>
+        </GridToolbarContainer>
+    );
+}
+
+
+const UsersManager = () => {
+    const { orgId, orgDoc } = useSpeerOrg();
+    const [users = [], loading, error] = useCollectionData(query(publicUserCollection, where('organization', '==', orgId)));
+    const [openDialog, closeDialog] = useDialog();
+    const { enqueueSnackbar } = useSnackbar();
+
+    const confirmDelete = (user: PublicUserDoc) => () => {
+        console.log(user);
+        openDialog({
+            children: <>
+                <DialogTitle>Confirm Action</DialogTitle>
+                <DialogContent>
+                    <p>Are you sure you want to remove {user.name} from your organization?</p>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={closeDialog}>Cancel</Button>
+                    <Button color="error" onClick={() => {
+                        console.log('TODO: Add RemoveUserFromOrganization backend', user.id)
+                        return;
+                        httpsCallable(functions, 'RemoveUserFromOrganization')({
+                            userId: user.id,
+                            orgId
+                        }).then(() => {
+                            closeDialog();
+                        }).catch(e => {
+                            console.error(e);
+                            enqueueSnackbar(e.message, { variant: "error" });
+                        })
+                    }}>Remove</Button>
+                </DialogActions>
+            </>
+        })
+    }
+
+    const columns: GridColumns<PublicUserDoc> = [
+        { field: 'id', headerName: 'ID', width: 200 },
+        {
+            field: 'name',
+            headerName: 'Name',
+            width: 250,
+        },
+        {
+            field: 'email',
+            headerName: 'Email',
+            width: 250,
+        },
+        {
+            field: 'role',
+            headerName: 'Role',
+            description: 'This column has a value getter and is not sortable.',
+            width: 160,
+            valueGetter: (params: GridValueGetterParams) => orgDoc?.permissions?.[params.row.id] || 'member'
+        },
+        {
+            field: 'actions',
+            type: 'actions',
+            getActions: (params: GridRowParams<PublicUserDoc>) => [
+                <GridActionsCellItem color="info" icon={<SecurityOutlined />} onClick={console.log} label="Edit Role" />,
+                <GridActionsCellItem color="error" icon={<Delete />} onClick={confirmDelete(params.row)} label="Delete" />
+            ]
+        }
+    ];
+
+    return (
+        <div className="space-y-4">
+            <h1>Users Manager</h1>
+            <DataGrid
+                autoHeight
+                loading={loading}
+                error={error}
+                rows={users}
+                columns={columns}
+                density={'compact'}
+                checkboxSelection
+                disableSelectionOnClick
+                components={{
+                    Toolbar: CustomToolbar
+                }}
+            />
+        </div>
+    );
+}
+export default UsersManager;
