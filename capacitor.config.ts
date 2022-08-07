3e717782ded71ed4f1b333497b6b76914ebf699a
ImJustChew <chewtzihwee@gmail.com>
2022-08-07 20:39:14 +0800

    Added capacitor js to out repository

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.speeredu.mobile',
+  appName: 'Speer Education',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
