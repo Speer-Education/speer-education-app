73b558c110a1d6432c942f40b55b8f9e866baa20
ImJustChew <chewtzihwee@gmail.com>
2022-08-07 20:45:02 +0800

    Rename to Insights Closes #114

--- src/components/Profile/ProfileViewsCard.tsx
+++ src/components/Profile/ProfileViewsCard.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const ProfileViewsCard: FC<{ views: number }> = ({ views }) => {
     return <div className="flex flex-col p-3 m-2 shadow-lg rounded-md bg-white space-y-3">
-        <h3 className="font-semibold text-lg">Lifetime Data</h3>
+        <h3 className="font-semibold text-lg">Insights</h3>
         <div className="flex flex-row items-center space-x-2">
             <p className="text-3xl w-16 text-center">{views}</p>
             <p className="text-gray-600">{"Profile Views"}</p>
