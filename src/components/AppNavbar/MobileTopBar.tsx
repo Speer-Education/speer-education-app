c55704107f1ad7fe172ee64495bdd5288f41e678
ImJustChew <chewtzihwee@gmail.com>
2022-08-08 08:39:15 +0800

    changed top app bar hiding breakpoint

--- src/components/AppNavbar/MobileTopBar.tsx
+++ src/components/AppNavbar/MobileTopBar.tsx
@@ -20,7 +20,7 @@ const MobileTopBar = () => {
     }
 
     return (
-        <div className="sticky md:hidden top-0 left-0 w-screen h-14 bg-white z-50 shadow-md">
+        <div className="sticky lg:hidden top-0 left-0 w-screen h-14 bg-white z-50 shadow-md">
             <div className="flex flex-row items-center h-full justify-between">
                 <img className="-mb-1 h-14 cursor-pointer pr-4" src="/full-transparent-logo.png" alt="logo" onClick={() => navigate('/')} />
                 <div className='flex flex-row space-x-1 items-center'>
