b606dcc75d3340d6805f1c8e7f16ea1d98b6389e
Ee-Hsin <eehsinkok777@gmail.com>
2022-08-14 18:22:50 +0800

    Added Smartlook analytics and Amplitude

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -2,6 +2,11 @@ import { isDevelopment } from './environment';
 import { analytics } from '../config/firebase';
 import mixpanel from '../config/mixpanel';
 import { logEvent as logEventToAnalytics } from 'firebase/analytics'
+import * as amplitude from '@amplitude/analytics-browser';
+import smartlookClient from 'smartlook-client';
+
+amplitude.init('627e19822eb4482290feebf284cd9f4c');
+smartlookClient.init('784a0552bf8bbf15124518cb462f45113638da83');
 
 const logEvent = (eventName: string, eventData: any = null) => {
     
@@ -9,6 +14,8 @@ const logEvent = (eventName: string, eventData: any = null) => {
     if(isDevelopment() || !analytics) return;
     logEventToAnalytics(analytics, eventName, eventData);
     mixpanel.track(eventName, eventData);
+    amplitude.track(eventName, eventData);
+    smartlookClient.track(eventName, eventData);
 }
 
 const setUserProperties = (uid: string, userDetails: any) => {
@@ -21,6 +28,15 @@ const setUserProperties = (uid: string, userDetails: any) => {
     mixpanel.identify(uid);
     //@ts-ignore
     mixpanel.people.set(userDetails);
+
+    //Amplitude
+    const event = new amplitude.Identify();
+    event.set(uid, userDetails);
+    amplitude.identify(event);
+
+    //Smartlook
+    smartlookClient.identify(uid, userDetails);
+
 }
 
 
